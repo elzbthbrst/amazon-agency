2ef617c02462db873bc8acefec8818b7e457d558
Yelyzaveta Berest <lizaberest97@gmail.com>
2024-01-18 12:46:48 +0200

    feat: routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { Contact } from "./pages/Contact.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/amazon-agency/",
+    element: <App />,
+    children: [
+      {
+        path: "/amazon-agency/",
+        element: <Home />,
+      },
+      {
+        path: "/amazon-agency/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
